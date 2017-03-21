1a06c58ed80270c6b4f808237120526d2a4ea4b6
John Tonkin <john.william.tonkin@gmail.com>
2017-03-21 23:57:01 +1030

    attempting to fix issue with headers

--- src/actions/GamesAPI.js
+++ src/actions/GamesAPI.js
@@ -4,7 +4,8 @@ export default {
 
   newGame: () => {
     return fetch("http://ec2-13-54-124-87.ap-southeast-2.compute.amazonaws.com/api/Games/newGame", {
-        method: "POST"
+        method: "POST",
+        compress: false
       })
       .then(function(response) {
         return response.json();
