53a2a3534a05469b9e03962456fd4fc584835bbf
John Tonkin <john.william.tonkin@gmail.com>
2017-03-21 23:30:43 +1030

    update to ec2 api server

--- src/actions/GamesAPI.js
+++ src/actions/GamesAPI.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 export default {
 
   newGame: () => {
-    return fetch("http://localhost:3001/api/Games/newGame", {
+    return fetch("https://ec2-13-54-124-87.ap-southeast-2.compute.amazonaws.com/api/Games/newGame", {
         method: "POST"
       })
       .then(function(response) {
